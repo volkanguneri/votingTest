44d7c43f2b87860ca4723e36dc68791cb3e8d2a7
root <root@DESKTOP-IA0TRAI>
2023-10-27 11:42:19 +0200

    10 test unitaires fonctionnent pour les sections: getters et voters registration

--- test/Voting.test..js
+++ test/Voting.test..js
@@ -7,13 +7,14 @@ const { ethers } = require("hardhat");
 const {
   isCallTrace,
 } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+const { LogDescription } = require("ethers");
 
 describe("Voting", async function () {
-  let voting, owner, voter1, voter2;
+  let voting, owner, voter, voter2;
 
   describe("Getters", async function () {
     beforeEach(async function () {
-      [owner, voter1, voter2, isNotVoter] = await ethers.getSigners();
+      [owner, voter, voter2, isNotVoter] = await ethers.getSigners();
       const contract = await hre.ethers.getContractFactory("Voting");
       voting = await contract.deploy(owner);
     });
@@ -23,61 +24,89 @@ describe("Voting", async function () {
       assert.equal(owner.address, _owner, "The owner address doesn't match");
     });
 
-    it("If NOT voter should not get voter adresses", async function () {
+    it("If NOT voter should NOT get voter adresses", async function () {
       await expect(
         voting.connect(isNotVoter).getVoter(owner.address)
       ).to.be.revertedWith("You're not a voter");
     });
 
     it("Voters should get adresses", async function () {
-      await voting.connect(owner).addVoter(voter1.address);
-      expect(voting.connect(voter1).getVoter(owner.address));
+      await voting.addVoter(voter.address);
+      expect(voting.connect(voter).getVoter(owner.address));
     });
 
-    // it("voters should get proposals", async function () {
-    //   let anyProposal;
-    //   await voting.connect(owner).addProposal(anyProposal);
-    //   expect(voting.connect(owner).getOneProposal()[0]); //.to.be.revertedWith("You're not a voter");
-    // });
-
     it("voters should get proposals", async function () {
-      let anyProposal;
+      await voting.addVoter(owner.address);
+      await voting.startProposalsRegistering();
+      const [description, voteCount] = await voting
+        .connect(owner)
+        .getOneProposal(0);
+      expect(description).to.equal("GENESIS");
+    });
 
-      // Add a proposal and capture the proposal ID
-      let proposalId = await voting.connect(owner).addProposal(anyProposal);
-
-      // Retrieve the proposal with the captured proposal ID
-      let proposal = await voting.connect(owner).getOneProposal(proposalId);
-
-      // Now, you can make assertions about the retrieved proposal
-      expect(proposal[0]).to.equal("Your Expected Proposal Data");
+    it("If NOT voter, should NOT get proposals", async function () {
+      await expect(
+        voting.connect(isNotVoter).getOneProposal(0)
+      ).to.be.revertedWith("You're not a voter");
     });
   });
 
-  // describe("Registration", async function () {
-  //   beforeEach(async function () {
-  //     [owner, voter1, voter2] = await ethers.getSigners();
+  describe("Registration", async function () {
+    beforeEach(async function () {
+      [owner, voter, voter2] = await ethers.getSigners();
 
-  //     const contract = await hre.ethers.getContractFactory("Voting");
-  //     voting = await contract.deploy();
-  //   });
+      const contract = await hre.ethers.getContractFactory("Voting");
+      voting = await contract.deploy();
+    });
 
-  //   // it("Check if the registration session is open", async function () {});
+    it("Registration session should be open to add voters", async function () {
+      await voting.startProposalsRegistering();
+      let registeringStatus = await voting.workflowStatus();
+      if (registeringStatus !== 0) {
+        await expect(voting.addVoter(isNotVoter.address)).to.be.revertedWith(
+          "Voters registration is not open yet"
+        );
+      }
+    });
 
-  // it("Only the owner should add voters", async function () {
-  //   await expect(
-  //     voting.connect(voter1).addVoter(voter2.address)
-  //   ).to.be.revertedWithCustomError(voting, "OwnableUnauthorizedAccount");
-  // });
+    it("if NOT owner, should NOT add voters", async function () {
+      await expect(
+        voting.connect(isNotVoter).addVoter(voter.address)
+      ).to.be.revertedWithCustomError(voting, "OwnableUnauthorizedAccount");
+    });
 
-  //   it("Check if the owner can add voters", async function () {
-  //     await voting.connect(owner.address).addVoter(voter2.address);
-  //     const _voter2 = await voting.getVoter(voter2.address);
-  //     expect(_voter2.isRegistered).to.equal(true);
-  //   });
+    it("if owner and registeration session's open, owner should add voters", async function () {
+      await voting.addVoter(owner.address);
+      await voting.addVoter(voter.address);
+      const voterInfo = await voting.getVoter(voter.address);
+      expect(voterInfo.isRegistered).to.equal(true);
+    });
 
-  //   // it("");
-  // });
+    it("already registered as voter should NOT be registered twice", async function () {
+      await voting.addVoter(owner.address);
+      await voting.addVoter(voter.address);
+      await expect(voting.addVoter(voter.address)).to.be.revertedWith(
+        "Already registered"
+      );
+    });
+
+    // it("should emit an event if the owner deposits ethers", async function () {
+    //   let etherQuantity = ethers.parseEther("0.1");
+    //   await expect(bank.deposit({ value: etherQuantity }))
+    //     .to.emit(bank, "Deposit")
+    //     .withArgs(owner.address, etherQuantity);
+
+    //   let balanceOfBank = await ethers.provider.getBalance(bank.target);
+    //   assert.equal(balanceOfBank.toString(), 100000000000000000);
+    // });
+
+    it("should emit VoterRegistered event", async function () {
+      await expect(voting.addVoter(voter.address))
+        .to.emit(voting, "VoterRegistered")
+        .withArgs(voter.address);
+    });
+  });
+
   // describe("Proposal", async function () {});
   // describe("Vote", async function () {});
   // describe("State", async function () {});
