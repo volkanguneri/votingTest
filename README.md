35147ad79db87a47e4cac2ab841a3e41021c57f2
root <root@DESKTOP-IA0TRAI>
2023-10-28 17:46:25 +0200

    addproposal and set vote functions are generally tested

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
 ```
+
+REPORT_GAS=true yarn hardhat test ./test/Storage.js
--- test/Voting.test..js
+++ test/Voting.test..js
@@ -8,9 +8,29 @@ const {
   isCallTrace,
 } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 const { LogDescription } = require("ethers");
+const {
+  TASK_COMPILE_REMOVE_OBSOLETE_ARTIFACTS,
+} = require("hardhat/builtin-tasks/task-names");
 
 describe("Voting", async function () {
   let voting, owner, voter, voter2;
+
+  // create a fixture to reload blockchain in beforeEach hooks. Attention to addVoter function additionnaly used in some of them.
+
+  // voter 1 and voter 2 sont ils nécessaires?
+
+  // toutes les exports en haut sont ils nécessaires?
+
+  beforeEach(async function () {
+    [owner, voter, voter2, isNotVoter] = await ethers.getSigners();
+    const contract = await hre.ethers.getContractFactory("Voting");
+    voting = await contract.deploy(owner);
+  });
+
+  it("Verify the owner of the contract", async function () {
+    const _owner = await voting.owner();
+    assert.equal(owner.address, _owner, "The owner address doesn't match");
+  });
 
   describe("Getters", async function () {
     beforeEach(async function () {
@@ -19,12 +39,7 @@ describe("Voting", async function () {
       voting = await contract.deploy(owner);
     });
 
-    it("Verify the owner of the contract", async function () {
-      const _owner = await voting.owner();
-      assert.equal(owner.address, _owner, "The owner address doesn't match");
-    });
-
-    it("If NOT voter should NOT get voter adresses", async function () {
+    it("should NOT get voter adresses if NOT voter ", async function () {
       await expect(
         voting.connect(isNotVoter).getVoter(owner.address)
       ).to.be.revertedWith("You're not a voter");
@@ -44,7 +59,7 @@ describe("Voting", async function () {
       expect(description).to.equal("GENESIS");
     });
 
-    it("If NOT voter, should NOT get proposals", async function () {
+    it("should NOT get proposals if NOT voter, ", async function () {
       await expect(
         voting.connect(isNotVoter).getOneProposal(0)
       ).to.be.revertedWith("You're not a voter");
@@ -59,7 +74,7 @@ describe("Voting", async function () {
       voting = await contract.deploy();
     });
 
-    it("Registration session should be open to add voters", async function () {
+    it("Voters registration session should be open to add voters", async function () {
       await voting.startProposalsRegistering();
       let registeringStatus = await voting.workflowStatus();
       if (registeringStatus !== 0) {
@@ -75,7 +90,7 @@ describe("Voting", async function () {
       ).to.be.revertedWithCustomError(voting, "OwnableUnauthorizedAccount");
     });
 
-    it("if owner and registeration session's open, owner should add voters", async function () {
+    it("owner should add voters", async function () {
       await voting.addVoter(owner.address);
       await voting.addVoter(voter.address);
       const voterInfo = await voting.getVoter(voter.address);
@@ -90,16 +105,6 @@ describe("Voting", async function () {
       );
     });
 
-    // it("should emit an event if the owner deposits ethers", async function () {
-    //   let etherQuantity = ethers.parseEther("0.1");
-    //   await expect(bank.deposit({ value: etherQuantity }))
-    //     .to.emit(bank, "Deposit")
-    //     .withArgs(owner.address, etherQuantity);
-
-    //   let balanceOfBank = await ethers.provider.getBalance(bank.target);
-    //   assert.equal(balanceOfBank.toString(), 100000000000000000);
-    // });
-
     it("should emit VoterRegistered event", async function () {
       await expect(voting.addVoter(voter.address))
         .to.emit(voting, "VoterRegistered")
@@ -107,7 +112,138 @@ describe("Voting", async function () {
     });
   });
 
-  // describe("Proposal", async function () {});
+  describe("Add proposal function", async function () {
+    let anyProposal;
+
+    beforeEach(async function () {
+      [owner, voter, voter2] = await ethers.getSigners();
+
+      const contract = await hre.ethers.getContractFactory("Voting");
+      voting = await contract.deploy();
+
+      await voting.addVoter(owner.address);
+    });
+
+    it("If NOT voter, should NOT get proposals", async function () {
+      anyProposal = "Any proposal but not an empty string";
+      await expect(
+        voting.connect(isNotVoter).addProposal(anyProposal)
+      ).to.be.revertedWith("You're not a voter");
+    });
+
+    it("Proposal registration session should be open to add proposals", async function () {
+      anyProposal = "Any proposal but not an empty string";
+      let registeringStatus = await voting.workflowStatus();
+      if (registeringStatus !== 1) {
+        await expect(voting.addProposal(anyProposal)).to.be.revertedWith(
+          "Proposals are not allowed yet"
+        );
+      }
+    });
+
+    it("Proposal can not be an empty string", async function () {
+      anyProposal = "";
+      await voting.startProposalsRegistering();
+      await expect(voting.addProposal(anyProposal)).to.be.revertedWith(
+        "Vous ne pouvez pas ne rien proposer"
+      );
+    });
+
+    it("Voters can add proposals", async function () {
+      anyProposal = "Any proposal but not an empty string";
+      await voting.startProposalsRegistering();
+      await voting.addProposal(anyProposal);
+      const [description, voteCount] = await voting.getOneProposal(1);
+      assert.equal(description, "Any proposal but not an empty string");
+    });
+
+    it("Should add more than one proposals"); // add like five proposals
+
+    it("should emit ProposalRegistered event", async function () {
+      anyProposal = "Any proposal but not an empty string";
+      await voting.startProposalsRegistering();
+      await expect(voting.addProposal(anyProposal))
+        .to.emit(voting, "ProposalRegistered")
+        .withArgs(1);
+    });
+  });
+
+  describe("Vote function", async function () {
+    beforeEach(async function () {
+      [owner, voter, voter2] = await ethers.getSigners();
+
+      const contract = await hre.ethers.getContractFactory("Voting");
+      voting = await contract.deploy();
+
+      await voting.addVoter(owner.address);
+
+      await voting.startProposalsRegistering();
+    });
+
+    it("shoul NOT vote if not registered as voter", async function () {
+      await expect(voting.connect(isNotVoter).setVote(0)).to.be.revertedWith(
+        "You're not a voter"
+      );
+    });
+
+    it("Voting session has to be started", async function () {
+      anyProposal = "Any proposal but not an empty string";
+      let registeringStatus = await voting.workflowStatus();
+      if (registeringStatus !== 2) {
+        await expect(voting.setVote(0)).to.be.revertedWith(
+          "Voting session havent started yet"
+        );
+      }
+    });
+
+    it("should NOT vote more than once", async function () {
+      await voting.endProposalsRegistering();
+      await voting.startVotingSession();
+      await voting.setVote(0);
+      await expect(voting.setVote(0)).to.be.revertedWith(
+        "You have already voted"
+      );
+    });
+
+    it("Proposals id should NOT be more or equal to proposals length", async function () {
+      await voting.endProposalsRegistering();
+      await voting.startVotingSession();
+      await expect(voting.setVote(1)).to.be.revertedWith("Proposal not found");
+    });
+
+    it("voters should vote for the GENESIS proposal", async function () {
+      await voting.endProposalsRegistering();
+      await voting.startVotingSession();
+      await voting.setVote(0);
+      let [isRegistered, hasVoted, votedProposalId] = await voting.getVoter(
+        owner.address
+      );
+      assert.equal(votedProposalId, 0);
+    });
+
+    it("voters should vote for an other proposal", async function () {
+      anyProposal = "Any proposal but not an empty string";
+      await voting.addProposal(anyProposal);
+      await voting.endProposalsRegistering();
+      await voting.startVotingSession();
+      await voting.setVote(1);
+      let [isRegistered, hasVoted, votedProposalId] = await voting.getVoter(
+        owner.address
+      );
+      assert.equal(votedProposalId, 1);
+    });
+
+    it("should emit Voted event", async function () {
+      anyProposal = "Any proposal but not an empty string";
+      await voting.addProposal(anyProposal);
+      await voting.endProposalsRegistering();
+      await voting.startVotingSession();
+      await expect(voting.setVote(1))
+        .to.emit(voting, "Voted")
+        .withArgs(owner.address, 1);
+    });
+  });
+
   // describe("Vote", async function () {});
   // describe("State", async function () {});
 });
